a7dcc8994fe784ae486492dedf8d610fb6a8003c
dragshooter <89727012+dragshooter@users.noreply.github.com>
2021-09-01 19:00:31 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -32,7 +32,7 @@ export default class Command extends BaseCommand {
                     categories[info.config.category].push(info)
                 }
             }
-            let text = `👾 *Hiro's Command List* 👾\n\n`
+                let text = `👾 *well's Command List* 👾\n\n`
             const keys = Object.keys(categories)
             for (const key of keys)
                 text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}*\n❐ \`\`\`${categories[
